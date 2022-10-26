f13429335327b1a0ea78e5f2faaf8e1b707f0c60
Deximus-Maximus <kev12east@gmail.com>
2022-10-26 15:36:49 -0400

    Tests

--- src/test/java/dex/plugins/OutletPluginTest.java
+++ src/test/java/dex/plugins/OutletPluginTest.java
@@ -13,8 +13,9 @@ public class OutletPluginTest {
         Project project = ProjectBuilder.builder().build();
         project.getPluginManager().apply("dex.plugins.outlet");
         project.getExtensions().configure(OutletExtension.class, (a) -> {
-            a.mcVersionRange = ">=1.18.2-alpha.22.6.a";
+            a.mcVersionRange = ">=1.18.2-alpha.22.6.a";//">=1.18 <1.19-"
             a.propertiesData.put("meh", 1);
+            a.allowSnapshotsForProject = true;
             a.propertiesFile = new File("gradle.properties");
         });
         System.out.println(project.getExtensions().getByType(OutletExtension.class).latestMc());
@@ -22,6 +23,8 @@ public class OutletPluginTest {
         System.out.println(project.getExtensions().getByType(OutletExtension.class).javaVersion());
         System.out.println(project.getExtensions().getByType(OutletExtension.class).curseforgeMcVersions());
         System.out.println(project.getExtensions().getByType(OutletExtension.class).javaLanguageCompatibility());
+        System.out.println(project.getExtensions().getByType(OutletExtension.class).fapiVersion());
+        //project.getExtensions().getByType(OutletExtension.class).meh();
         assert project.getExtensions().getByType(OutletExtension.class).mcVersions() != null;
         assert project.getExtensions().getByType(OutletExtension.class).fapiVersion() != null;
         assert project.getExtensions().getByType(OutletExtension.class).curseforgeMcVersions() != null;
