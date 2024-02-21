43e81e5eaf27fecc2717a2b6f6ef0f2bd76b934d
Deximus-Maximus <kev12east@gmail.com>
2024-02-21 00:18:02 -0500

    Allow plugin to be applied in settings

--- src/main/groovy/dex/plugins/OutletPlugin.groovy
+++ src/main/groovy/dex/plugins/OutletPlugin.groovy
@@ -2,10 +2,20 @@ package dex.plugins
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.initialization.Settings
+import org.gradle.api.plugins.PluginAware
 import org.gradle.api.tasks.TaskContainer
 
-class OutletPlugin implements Plugin<Project> {
+class OutletPlugin implements Plugin<PluginAware> {
     @Override
+    void apply(final PluginAware pluginAware) {
+        if (pluginAware instanceof Project) {
+            apply((Project)pluginAware)
+        } else if (pluginAware instanceof Settings) {
+            apply((Settings)pluginAware)
+        }
+    }
+
     void apply(final Project project) {
         def ext = project.getExtensions().create("outlet", OutletExtension.class, project)
 
@@ -20,5 +30,9 @@ class OutletPlugin implements Plugin<Project> {
             // This handles runClient and Idea run configs
             tasks.classes.finalizedBy task
         }
+    }
+
+    void apply(final Settings settings) {//todo will this conflict with other setting in project?
+        def ext = settings.getExtensions().create("outlet", OutletExtension.class, settings)
     }
 }
